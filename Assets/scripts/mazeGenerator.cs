00491062fde29d75b6f27e3f344f099150edcca6
Xinyi Wang <xinyi@Xinyis-MacBook-Pro.local>
2016-05-01 20:43:17 -0500

    needs to polish maze

--- Assets/scripts/mazeGenerator.cs
+++ Assets/scripts/mazeGenerator.cs
@@ -11,14 +11,16 @@ public class mazeGenerator : MonoBehaviour {
 	public GameObject wall;
 	public GameObject sphere;
 	public GameObject mCamera;
+	public GameObject redBrick;
+	public GameObject blueBrick;
 
 	private Vector3 ballPos;
 	private Vector3 cameraPos;
-
 	private static int coefficient = -1;
 	private static List<Vector2> offsets = new List<Vector2> { new Vector2(0, 1), new Vector2(0, -1), new Vector2(1, 0), new Vector2(-1, 0) };
 	private System.Random rnd = new System.Random();
 	private Stack<Vector2> stack = new Stack<Vector2>();
+	public GameObject winObject;
 
 	private Vector2 _currentTile;
 	public Vector2 CurrentTile
@@ -28,8 +30,6 @@ public class mazeGenerator : MonoBehaviour {
 		{
 			if (value.x < 1 || value.x >= this.mazeWidth - 1 || value.y < 1 || value.y >= this.mazeHeight - 1)
 			{
-				print (value.x);
-				print (value.y);
 				throw new ArgumentException("CurrentTile must be within the one tile border all around the maze");
 			}
 			if (value.x % 2 == 1 || value.y % 2 == 1)
@@ -42,7 +42,7 @@ public class mazeGenerator : MonoBehaviour {
 	}
 
 	void Start() {
-		ballPos = new Vector3 (mazeWidth / 2, 0.0f, 0.0f);
+		ballPos = new Vector3 (mazeWidth / 2.0f - 1.0f, mazeHeight / 2.0f - 1.0f, 0.0f);
 		cameraPos = new Vector3 (0.0f, 0.0f, -10.0f);
 		maze = new int[mazeWidth, mazeHeight];
 		GameObject sphereBall = Instantiate (sphere, ballPos, Quaternion.identity) as GameObject;
@@ -53,8 +53,9 @@ public class mazeGenerator : MonoBehaviour {
 	}
 
 	private void createFramework() {
-		Vector3 pos = new Vector3 (mazeWidth / 2, mazeHeight / 2, 0);
-		Vector3 scale = new Vector3 (mazeWidth, 1.0f, 1.1f);
+		Vector3 pos = new Vector3 (mazeWidth / 2.0f, mazeHeight / 2.0f, 0.0f);
+		Vector3 scale = new Vector3 (mazeWidth + 2.0f, 1.0f, 1.1f);
+		Vector3 scaleVerticle = new Vector3 (mazeHeight + 2.0f, 1.0f, 1.1f);
 		GameObject wallUp = Instantiate(wall, pos, Quaternion.identity) as GameObject;
 		wallUp.transform.localScale = scale;
 		pos.y *= coefficient;
@@ -62,13 +63,14 @@ public class mazeGenerator : MonoBehaviour {
 		wallDown.transform.localScale = scale;
 		GameObject wallRight = Instantiate (wall, new Vector3 (0, 0, 0), 
 			Quaternion.AngleAxis (90, new Vector3 (0, 0, 1))) as GameObject;
-		wallRight.transform.localScale = scale;
+		wallRight.transform.localScale = scaleVerticle;
 		GameObject wallLeft = Instantiate(wall, new Vector3(mazeWidth, 0, 0), 
 			Quaternion.AngleAxis (90, new Vector3 (0, 0, 1))) as GameObject;
-		wallLeft.transform.localScale = scale;
+		wallLeft.transform.localScale = scaleVerticle;
+		Instantiate (winObject, new Vector3 (1.5f, -mazeHeight / 2.0f + 1.5f, 0.0f), Quaternion.identity);
 	}
 
-
+	enum nextToGenerate {WALL, RED, BLUE};
 	private void createMaze(int difficulty) {
 		createFramework ();
 		for (int i = 0; i < mazeWidth; i++) {
@@ -79,12 +81,52 @@ public class mazeGenerator : MonoBehaviour {
 		CurrentTile = Vector2.one;
 		stack.Push (CurrentTile);
 		maze = fillTwoDimensionArray ();
+		Vector3 scale = new Vector3 (1.0f, 0.5f, 1.0f);
+		// maximum number of bricks in the maze
+		int maximumBlackWallNumber = difficulty * 15;
+		int count = 0;
 		for (int i = 0; i < mazeWidth; i++) {
 			for (int j = 0; j < mazeHeight; j++) {
-				print (maze [i, j]);
+				if (maze [i, j] == 1) {
+					bool boundary = checkBoundary (i, j);
+					Vector3 pos = new Vector3 (i, j - mazeHeight / 2.0f + 1.0f, 0);
+					if (boundary) {
+						continue;
+					}
+					nextToGenerate n = findNextToGenerate(count, maximumBlackWallNumber);
+					if (n == nextToGenerate.WALL && count < maximumBlackWallNumber) {
+						GameObject blackWall = Instantiate (wall, pos, Quaternion.identity) as GameObject;
+						blackWall.transform.localScale = scale;
+						count++;
+					} else if (n == nextToGenerate.RED) {
+						GameObject redWall = Instantiate (redBrick, pos, Quaternion.identity) as GameObject;
+						redWall.transform.localScale = scale;
+					} else {
+						GameObject blueWall = Instantiate (blueBrick, pos, Quaternion.identity) as GameObject;
+						blueWall.transform.localScale = scale;
+					}
+				}
 			}
-			print ("\n");
 		}
+	}
+
+
+	bool checkBoundary(int i, int j) {
+		return j == 0 || j == mazeHeight - 1 /*|| i == 0 || i == mazeWidth - 1*/;
+	}
+
+
+	nextToGenerate findNextToGenerate(int count, int maximumBlackWallNumber) {
+		nextToGenerate n;
+		int next = rnd.Next (maximumBlackWallNumber + 10);
+		if (next < maximumBlackWallNumber && count < maximumBlackWallNumber) {
+			n = nextToGenerate.WALL;
+		} else if (next < maximumBlackWallNumber + 5) {
+			n = nextToGenerate.BLUE;
+		} else {
+			n = nextToGenerate.RED;
+		}
+		return n;
 	}
 
 
@@ -96,7 +138,6 @@ public class mazeGenerator : MonoBehaviour {
 			if (neighbours.Count > 0) {
 				stack.Push (CurrentTile);
 				int next = rnd.Next (neighbours.Count);
-				print (neighbours [next]);
 				CurrentTile = neighbours [next];
 			} else {
 				CurrentTile = stack.Pop ();
